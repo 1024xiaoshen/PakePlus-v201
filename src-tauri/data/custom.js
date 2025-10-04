1c6e971c60b3c9857275e001f9dc5928ffab301a
1024xiaoshen <95572266+1024xiaoshen@users.noreply.github.com>
2025-10-04 17:32:47 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('555')
-    document.querySelector('body').innerHTML = 'PakePlus'
-    document.querySelector('body').style.backgroundColor = 'skyblue'
-})
